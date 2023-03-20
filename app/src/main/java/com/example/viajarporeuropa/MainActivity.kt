8a0bfc44bead54c6532be8605a640c9fca4b7dfc
equus2001 <62957466+equus2001@users.noreply.github.com>
2023-03-20 12:45:54 +0100

    Registro y principio de base de datos para la autentificación y del registro

--- app/src/main/java/com/example/viajarporeuropa/MainActivity.kt
+++ app/src/main/java/com/example/viajarporeuropa/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.viajarporeuropa
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.login).setOnClickListener {
+            var intento = Intent(this, Login::class.java)
+            startActivity(intento)
+        }
+
+        findViewById<Button>(R.id.registrar).setOnClickListener {
+            var intento = Intent(this, Registrar::class.java)
+            startActivity(intento)
+        }
     }
 }
