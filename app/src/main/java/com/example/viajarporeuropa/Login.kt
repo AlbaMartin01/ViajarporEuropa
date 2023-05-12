0eb2a6ac3d722c07801259e60f79e9034be89ff4
AlbaMartin01 <albmar891@alu.centroafuera.es>
2023-05-12 09:09:50 +0200

    pantalla usuario

--- app/src/main/java/com/example/viajarporeuropa/Login.kt
+++ app/src/main/java/com/example/viajarporeuropa/Login.kt
@@ -15,8 +15,6 @@ class Login : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
-    @SuppressLint("MissingInflatedId")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -28,20 +26,14 @@ class Login : AppCompatActivity() {
         btningresar.setOnClickListener{
             singIn(txtemail.text.toString(),txtpassword.text.toString())
         }
-
-        findViewById<Button>(R.id.textOlvidarContraseña).setOnClickListener {
-            val user = firebaseAuth.currentUser
-            firebaseAuth.sendPasswordResetEmail(user?.email.toString())
-        }
     }
 
     private fun singIn (email: String, password: String){
         firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
             if(task.isSuccessful){
                 val user = firebaseAuth.currentUser
-                Toast.makeText(this, user?.email.toString(), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, user?.uid.toString(), Toast.LENGTH_SHORT).show()
                 val intento = Intent(this, ListaPaisesEU::class.java)
-                intento.putExtra("nombreUsuario", user?.email.toString())
                 startActivity(intento)
             } else{
                 Toast.makeText(this, "Error de Email y/o contraseña", Toast.LENGTH_SHORT).show()
